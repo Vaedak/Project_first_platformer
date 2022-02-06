7bda25d7267295107fc50d0641db51e730749bed
Vaedak <67328279+Vaedak@users.noreply.github.com>
2022-02-06 16:08:20 +0100

    Add files via upload

---
+++ PROJECT_/asset_and_map/map/perso.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="perso" tilewidth="32" tileheight="32" tilecount="52" columns="26">
+ <image source="../../code/assets/perso.png" width="832" height="64"/>
+</tileset>
