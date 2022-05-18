d69bccad642cdf3e7d795bb7157367985a99ce01
Vaedak <67328279+Vaedak@users.noreply.github.com>
2022-05-18 12:57:13 +0200

    Add files via upload

---
+++ PigState/envi_asset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="envi_asset" tilewidth="16" tileheight="16" tilecount="6084" columns="78">
+ <image source="assets/envi_asset.png" width="1250" height="1250"/>
+</tileset>
